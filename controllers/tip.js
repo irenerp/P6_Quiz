8b0f45e55064c30988ef64731e66b613dce57228
Irene Ramirez Panduro <irene.ramirez.panduro@alumnos.upm.es>
2018-06-10 19:36:52 +0200

    quiz_2018

--- controllers/tip.js
+++ controllers/tip.js
@@ -5,48 +5,42 @@ const {models} = require("../models");
 // Autoload the tip with id equals to :tipId
 exports.load = (req, res, next, tipId) => {
 
-
-    models.quiz.findById(tipId)
-    
-    .then(tip=>{
-        if(tip){
-            req.tip = tip;
-            next();
-        }else{
-            next(new Error ('There is no tip with tipId=' + tipId));
-        }
-    })
-
-    .catch(error => next(error));
+    models.tip.findById(tipId)
+        .then(tip => {
+            if (tip) {
+                req.tip = tip;
+                next();
+            } else {
+                next(new Error('There is no tip with tipId=' + tipId));
+            }
+        })
+        .catch(error => next(error));
 };
 
 
 // POST /quizzes/:quizId/tips
 exports.create = (req, res, next) => {
 
-    const authorId = req.session.user && req.session.user.id || 0;
- 
     const tip = models.tip.build(
         {
             text: req.body.text,
-            quizId: req.quiz.id,
-            authorId
+            quizId: req.quiz.id
         });
 
     tip.save()
-    .then(tip => {
-        req.flash('success', 'Tip created successfully.');
-        res.redirect("back");
-    })
-    .catch(Sequelize.ValidationError, error => {
-        req.flash('error', 'There are errors in the form:');
-        error.errors.forEach(({message}) => req.flash('error', message));
-        res.redirect("back");
-    })
-    .catch(error => {
-        req.flash('error', 'Error creating the new tip: ' + error.message);
-        next(error);
-    });
+        .then(tip => {
+            req.flash('success', 'Tip created successfully.');
+            res.redirect("back");
+        })
+        .catch(Sequelize.ValidationError, error => {
+            req.flash('error', 'There are errors in the form:');
+            error.errors.forEach(({message}) => req.flash('error', message));
+            res.redirect("back");
+        })
+        .catch(error => {
+            req.flash('error', 'Error creating the new tip: ' + error.message);
+            next(error);
+        });
 };
 
 
@@ -58,14 +52,14 @@ exports.accept = (req, res, next) => {
     tip.accepted = true;
 
     tip.save(["accepted"])
-    .then(tip => {
-        req.flash('success', 'Tip accepted successfully.');
-        res.redirect('/quizzes/' + req.params.quizId);
-    })
-    .catch(error => {
-        req.flash('error', 'Error accepting the tip: ' + error.message);
-        next(error);
-    });
+        .then(tip => {
+            req.flash('success', 'Tip accepted successfully.');
+            res.redirect('/quizzes/' + req.params.quizId);
+        })
+        .catch(error => {
+            req.flash('error', 'Error accepting the tip: ' + error.message);
+            next(error);
+        });
 };
 
 
@@ -73,46 +67,82 @@ exports.accept = (req, res, next) => {
 exports.destroy = (req, res, next) => {
 
     req.tip.destroy()
-    .then(() => {
-        req.flash('success', 'tip deleted successfully.');
-        res.redirect('/quizzes/' + req.params.quizId);
-    })
-    .catch(error => next(error));
+        .then(() => {
+            req.flash('success', 'tip deleted successfully.');
+            res.redirect('/quizzes/' + req.params.quizId);
+        })
+        .catch(error => next(error));const Sequelize = require("sequelize");
+const {models} = require("../models");
+
+
+// Autoload the tip with id equals to :tipId
+exports.load = (req, res, next, tipId) => {
+
+    models.tip.findById(tipId)
+        .then(tip => {
+            if (tip) {
+                req.tip = tip;
+                next();
+            } else {
+                next(new Error('There is no tip with tipId=' + tipId));
+            }
+        })
+        .catch(error => next(error));
 };
 
-exports.adminOrAuthorRequired = (req, res, next) => {
 
-    const isAdmin  = !!req.session.user.isAdmin;
-    const isAuthor = req.quiz.authorId === req.session.user.id;
+// POST /quizzes/:quizId/tips
+exports.create = (req, res, next) => {
 
-    if (isAdmin || isAuthor) {
-        next();
-    } else {
-        console.log('Prohibited operation: The logged in user is not the author of the quiz, nor an administrator.');
-        res.send(403);
-    }
+    const tip = models.tip.build(
+        {
+            text: req.body.text,
+            quizId: req.quiz.id
+        });
+
+    tip.save()
+        .then(tip => {
+            req.flash('success', 'Tip created successfully.');
+            res.redirect("back");
+        })
+        .catch(Sequelize.ValidationError, error => {
+            req.flash('error', 'There are errors in the form:');
+            error.errors.forEach(({message}) => req.flash('error', message));
+            res.redirect("back");
+        })
+        .catch(error => {
+            req.flash('error', 'Error creating the new tip: ' + error.message);
+            next(error);
+        });
 };
 
-exports.update = (req, res, next) => {
 
-    const {quiz, tip, body} = req;
+// GET /quizzes/:quizId/tips/:tipId/accept
+exports.accept = (req, res, next) => {
 
-    tip.text = body.text;
+    const {tip} = req;
 
-    tip.save({fields: ["text"]})
-    .then(tip =>{
-        rep.flash('success', 'Tip edited successfully');
-        res.redirect('/goback');
-    })
-    .catch(Sequelize.ValidationError, error =>{
-        req.flash('error', 'There are error in the form');
-        error.errors.forEach(({message})=> req.flash('error'));
-        res.render('tips/edit', {tip, quiz});
-    })
+    tip.accepted = true;
 
-    .catch(error => {
-        req.flash('error', 'Error editing the quiz:' + error.message);
-        next(error);
-    });
+    tip.save(["accepted"])
+        .then(tip => {
+            req.flash('success', 'Tip accepted successfully.');
+            res.redirect('/quizzes/' + req.params.quizId);
+        })
+        .catch(error => {
+            req.flash('error', 'Error accepting the tip: ' + error.message);
+            next(error);
+        });
 };
 
+
+// DELETE /quizzes/:quizId/tips/:tipId
+exports.destroy = (req, res, next) => {
+
+    req.tip.destroy()
+        .then(() => {
+            req.flash('success', 'tip deleted successfully.');
+            res.redirect('/quizzes/' + req.params.quizId);
+        })
+        .catch(error => next(error));
+};
