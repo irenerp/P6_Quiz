46e0551e624685289290eee9dca2115d838d3463
Irene Ramirez Panduro <irene.ramirez.panduro@alumnos.upm.es>
2018-06-11 14:32:07 +0200

    quiz_2018

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,10 @@
-
 const express = require('express');
 const router = express.Router();
-
-const multer  = require('multer');
-const upload = multer({ dest: './uploads/' });
 
 const quizController = require('../controllers/quiz');
 const tipController = require('../controllers/tip');
 const userController = require('../controllers/user');
 const sessionController = require('../controllers/session');
-const favouriteController = require('../controllers/favourite');
 
 //-----------------------------------------------------------
 
@@ -38,16 +33,17 @@ function saveBack(req, res, next) {
 // Restoration routes are GET routes that do not end in:
 //   /new, /edit, /play, /check, /session, or /:id.
 router.get([
-    '/','/author',
+    '/',
+    '/author',
     '/users',
     '/users/:id(\\d+)/quizzes',
-    '/quizzes'],                 saveBack);
+    '/quizzes'], saveBack);
 
 //-----------------------------------------------------------
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-    res.render('index');
+  res.render('index');
 });
 
 // Author page.
@@ -92,23 +88,21 @@ router.delete('/users/:userId(\\d+)',
     sessionController.adminOrMyselfRequired,
     userController.destroy);
 
-
 router.get('/users/:userId(\\d+)/quizzes',
     sessionController.loginRequired,
     quizController.index);
 
 
 // Routes for the resource /quizzes
-router.get('/quizzes.:format?',
+router.get('/quizzes',
     quizController.index);
-router.get('/quizzes/:quizId(\\d+).:format?',
+router.get('/quizzes/:quizId(\\d+)',
     quizController.show);
 router.get('/quizzes/new',
     sessionController.loginRequired,
     quizController.new);
 router.post('/quizzes',
     sessionController.loginRequired,
-    upload.single('image'),
     quizController.create);
 router.get('/quizzes/:quizId(\\d+)/edit',
     sessionController.loginRequired,
@@ -117,52 +111,19 @@ router.get('/quizzes/:quizId(\\d+)/edit',
 router.put('/quizzes/:quizId(\\d+)',
     sessionController.loginRequired,
     quizController.adminOrAuthorRequired,
-    upload.single('image'),
     quizController.update);
 router.delete('/quizzes/:quizId(\\d+)',
     sessionController.loginRequired,
     quizController.adminOrAuthorRequired,
     quizController.destroy);
 
-router.get('/quizzes/:quizId(\\d+)/play',
-    quizController.play);
-router.get('/quizzes/:quizId(\\d+)/check',
-    quizController.check);
-
-
-
-router.post('/quizzes/:quizId(\\d+)/tips',
-    sessionController.loginRequired,
-    tipController.create);
-router.put('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)/accept',
-    sessionController.loginRequired,
-    quizController.adminOrAuthorRequired,
-    tipController.accept);
-router.delete('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)',
-    sessionController.loginRequired,
-    quizController.adminOrAuthorRequired,
-    tipController.destroy);
-
-
-// Routes for the resource favourites of a user
-router.put('/users/:userId(\\d+)/favourites/:quizId(\\d+)',
-    sessionController.loginRequired,
-    sessionController.adminOrMyselfRequired,
-    favouriteController.add);
-router.delete('/users/:userId(\\d+)/favourites/:quizId(\\d+)',
-    sessionController.loginRequired,
-    sessionController.adminOrMyselfRequired,
-    favouriteController.del);
-
-
-
 router.get('/quizzes/:quizId(\\d+)/play',  quizController.play);
 router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
-//P6
+
 router.get('/quizzes/randomplay',  quizController.randomplay);
 router.get('/quizzes/randomcheck/:quizId(\\d+)', quizController.randomcheck);
 
-//tips
+
 
 router.post('/quizzes/:quizId(\\d+)/tips',
     sessionController.loginRequired,
@@ -176,7 +137,6 @@ router.delete('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)',
     quizController.adminOrAuthorRequired,
     tipController.destroy);
 
-// Routes P8_Quiz
 router.get('/quizzes/:quizId(\\d+)/tips/:tipId(\\d+)/edit',
     sessionController.loginRequired,
     tipController.adminOrAuthorRequired,
